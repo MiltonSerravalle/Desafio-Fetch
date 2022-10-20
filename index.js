1935782b5131e207c59f3c81de49b27ba69ae867
Milton Serravalle <milton.serravalle@hotmail.com>
2022-10-19 21:50:04 -0300

    1 er commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const container = document.getElementById("container")
+
+fetch("https://rickandmortyapi.com/api/character/1,2,3,4,5")
+.then(res => res.json())
+.then(data => {
+    data.forEach(producto => {
+        const div = document.createElement("div")
+        div.innerHTML = `<div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="${producto.image}">
+        <div class="card-body">
+        <h5 class="card-title">"${producto.name}" </h5>
+        <p class="card-text">"${producto.gender}"</p>
+        </div>
+        </div>
+        `
+        container.appendChild(div);
+    });
+})
+
+
+
+// const personajes = async () => {
+//     try{
+//     const resp = await fetch('https://rickandmortyapi.com/api/character/1,2,3,4,5')
+//     const data = await resp.json()
+
+//     data.forEach((personaje) => {
+//         const carta = document.createElement("div")
+//         div.innerHTML = `
+//         <img src="https://rickandmortyapi.com/api/character/1,2,3,4,5">
+//         <h4>"${personaje.name}"</h4>
+//         <p>"${personaje.gender}"</p>
+//         `
+
+//     container.append(div)
+//     })}
+//     catch{
+        
+//     }
+    
+// }
+
+// personajes ()
